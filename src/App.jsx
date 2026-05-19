41852a63f10df08f48a68cee6d6fd3ce04d9f10d
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-19 22:56:40 +0600

    Add loading Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { Suspense } from "react";
+import Countries from "./Countries";
+import Loading from "./Loading";
+import "./index.css";
+
+
+const fetchCountries = async () => {
+  const res = await fetch("https://openapi.programming-hero.com/api/all");
+  return res.json();
+}
+
 
 
 function App() {
-  
+const countriesPromise=fetchCountries()
 
   return (
     <>
-      
+      <Suspense fallback={<Loading/>}>
+        <Countries countriesPromise={countriesPromise} />
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
