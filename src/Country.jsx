5d42c88f3c07ac62bd6ebd7e909b292db5c2d12c
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-19 23:27:57 +0600

    Country Info Added

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,13 +1,17 @@
 
 
 const Country = ({ country }) => {
-    const { message } = country;
+    
 
-    return <div>
-        <p>
-           Message: {message}; 
-      </p>
-  </div>;
+    return (
+      <div>
+        <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+        <p>Name: {country.name.common}</p>
+            <p>Official Name: {country.name.official}</p>
+            <p>Region: {country.region.region }</p>
+            <p>Area: { country.area.area}</p>
+      </div>
+    );
 };
 
 export default Country;
