615f4ea348236720a9b67f45eb8ca7c22b0ec5e0
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-20 00:06:12 +0600

    Add Conditional Button

--- src/Country.jsx
+++ src/Country.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 
 
 const Country = ({ country }) => {
+    const [visited, setVisited]=useState(false)
+
+    const handelVisited=()=>{
+       setVisited(!visited)
+    }
     
 
     return (
-      <div className="border border-salmon p-6 space-y-3 ">
+      <div className={`border rounded-2xl p-6 space-y-3 ${visited ? "border-green-400" :  "border-red-800"}`}>
         <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
         <p>Name: {country.name.common}</p>
         <p>Official Name: {country.name.official}</p>
         <p>Region: {country.region.region}</p>
-        <p>Area: {country.area.area}</p>
-        <button className="btn btn-soft btn-accent">Visited</button>
+        <p className="">
+          Area: {country.area.area}
+          <span className="ml-2">
+            {country.area > 300000 ? "Big Country" : "Small Country"}
+          </span>
+        </p>
+        <button className={`btn ${visited? "btn-success": "btn-error"}`} onClick={handelVisited}>
+          {visited ? "Visited" : "Not Visited"}
+        </button>
       </div>
     );
 };
