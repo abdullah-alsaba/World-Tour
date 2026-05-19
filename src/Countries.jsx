490d57908fe58f71f7695d3fda185824a71260ae
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-20 00:32:34 +0600

    Total Functionality

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -1,8 +1,15 @@
-import { use } from "react";
+import { use, useState } from "react";
 import "./index.css";
 import Country from "./Country";
 
 const Countries = ({ countriesPromise }) => {
+
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handelVisitedCountries = (country) => {
+        const totalVisitedCountries = [...visitedCountries, country]
+        setVisitedCountries(totalVisitedCountries);
+    }
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries
 
@@ -11,10 +18,14 @@ const Countries = ({ countriesPromise }) => {
       <h1 className="text-4xl font-bold">
         Total Countries: {countries.length}{" "}
       </h1>
-      <h1 className="text-3xl font-bold">Countries i visited:</h1>
-      <div className="grid grid-cols-3 gap-4">
+          <h1 className="text-3xl font-bold">Total visited countries: { visitedCountries.length}</h1>
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
         {countries.map((country) => (
-          <Country country={country} key={country.ccn3.ccn3} />
+          <Country
+            country={country}
+            key={country.ccn3.ccn3}
+            handelVisitedCountries={handelVisitedCountries}
+          />
         ))}
       </div>
     </div>
--- src/Country.jsx
+++ src/Country.jsx
@@ -1,31 +1,36 @@
 import { useState } from "react";
 
 
-const Country = ({ country }) => {
-    const [visited, setVisited]=useState(false)
+const Country = ({ country, handelVisitedCountries }) => {
+  const [visited, setVisited] = useState(false);
 
-    const handelVisited=()=>{
-       setVisited(!visited)
-    }
-    
+  const handelVisited = () => {
+      setVisited(!visited);
+      handelVisitedCountries(country)
+  };
 
-    return (
-      <div className={`border rounded-2xl p-6 space-y-3 ${visited ? "border-green-400" :  "border-red-800"}`}>
-        <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
-        <p>Name: {country.name.common}</p>
-        <p>Official Name: {country.name.official}</p>
-        <p>Region: {country.region.region}</p>
-        <p className="">
-          Area: {country.area.area}
-          <span className="ml-2">
-            {country.area > 300000 ? "Big Country" : "Small Country"}
-          </span>
-        </p>
-        <button className={`btn ${visited? "btn-success": "btn-error"}`} onClick={handelVisited}>
-          {visited ? "Visited" : "Not Visited"}
-        </button>
-      </div>
-    );
+  return (
+    <div
+      className={`border rounded-2xl p-6 space-y-3 ${visited ? "border-green-400" : "border-red-800"}`}
+    >
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <p>Name: {country.name.common}</p>
+      <p>Official Name: {country.name.official}</p>
+      <p>Region: {country.region.region}</p>
+      <p className="">
+        Area: {country.area.area}
+        <span className="ml-2">
+          {country.area > 300000 ? "Big Country" : "Small Country"}
+        </span>
+      </p>
+      <button
+        className={`btn ${visited ? "btn-success" : "btn-error"}`}
+        onClick={handelVisited}
+      >
+        {visited ? "Visited" : "Not Visited"}
+      </button>
+    </div>
+  );
 };
 
 export default Country;
