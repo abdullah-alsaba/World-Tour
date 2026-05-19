5eac3379f09f8762306b34ca5e8b07ff8cfc8a35
Abdulla Al Saba <abdullaalsaba28@gmail.com>
2026-05-19 23:37:04 +0600

    UI Completed

--- src/Countries.jsx
+++ src/Countries.jsx
@@ -7,15 +7,16 @@ const Countries = ({ countriesPromise }) => {
     const countries = countriesData.countries
 
   return (
-    <div className="container mx-auto px-60 space-y-4 mt-20">
+    <div className="container mx-auto px-60 space-y-4 mt-20 ">
       <h1 className="text-4xl font-bold">
         Total Countries: {countries.length}{" "}
       </h1>
       <h1 className="text-3xl font-bold">Countries i visited:</h1>
-
-      {countries.map((country) => (
-        <Country country={country} key={country.ccn3.ccn3} />
-      ))}
+      <div className="grid grid-cols-3 gap-4">
+        {countries.map((country) => (
+          <Country country={country} key={country.ccn3.ccn3} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/Country.jsx
+++ src/Country.jsx
@@ -4,12 +4,13 @@ const Country = ({ country }) => {
     
 
     return (
-      <div>
+      <div className="border border-salmon p-6 space-y-3 ">
         <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
         <p>Name: {country.name.common}</p>
-            <p>Official Name: {country.name.official}</p>
-            <p>Region: {country.region.region }</p>
-            <p>Area: { country.area.area}</p>
+        <p>Official Name: {country.name.official}</p>
+        <p>Region: {country.region.region}</p>
+        <p>Area: {country.area.area}</p>
+        <button className="btn btn-soft btn-accent">Visited</button>
       </div>
     );
 };
